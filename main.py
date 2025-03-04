9490bf4804d0c752436335c0fb91ca0c7cbcb269
glen654 <glenalloy@gmail.com>
2025-03-04 12:36:54 +0530

    implemented text area to parse the content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
 
 st.title("AI Web Scrapper")
 url = st.text_input("Enter a website URL: ")
@@ -7,4 +7,20 @@ url = st.text_input("Enter a website URL: ")
 if st.button("Scrape Site"):
     st.write("Scraping the website")
     result = scrape_website(url)
-    print(result)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 import time
 
 def scrape_website(website):
@@ -22,3 +23,32 @@ def scrape_website(website):
         return html
     finally:
         driver.quit()
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    # getting rid of the style and script tags
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    # get all the text and seperated it with a new line
+    cleaned_content = soup.get_text(separator="\n")
+    # if \n character not seperatign anything if there's no text between it and the next thing then we're just gonna remove it
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
+# make the dom content up to 6000 batches
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i: i + max_length] for i in range(0,len(dom_content),max_length)
+    ]
